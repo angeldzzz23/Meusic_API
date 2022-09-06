7ed0839ec6e47825bc2adbf387ba8ddb5eb5c527
angel zambrano <angeldzambrano99@gmail.com>
2022-09-05 19:29:05 -0700

    added patch for genres

--- proyecto_musica/misc/views.py
+++ proyecto_musica/misc/views.py
@@ -228,6 +228,31 @@ class GenreView(GenericAPIView):
 
         return response.Response(res)
 
+    def patch(self, request, id):
+        jd = request.data
+        if request.user.is_superuser != True:
+            res = {'success' : False, 'error' : "You do not have access to edit objs"}
+            return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            you = Genres.objects.get(genre_id=id)
+
+        except:
+            res = {'success' : False, 'error' : "id does not exist"}
+            return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer=GenresSerializer(you,data=jd,partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            res = {'success' : False, 'error' : serializer.errors}
+            return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
+
+        res = {'success' : True, 'data': serializer.data}
+        return response.Response(res, status=status.HTTP_201_CREATED)
+
+
 
 class SpotifyPlatforms(GenericAPIView):
     permission_classes = (permissions.IsAuthenticated,)
