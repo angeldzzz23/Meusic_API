2cb61fe227dfe695317d5e222632d7544d349369
angel zambrano <angeldzambrano99@gmail.com>
2022-09-15 11:59:13 -0700

    added validation for video

--- proyecto_musica/api/views.py
+++ proyecto_musica/api/views.py
@@ -138,6 +138,20 @@ class UpdateVideo(GenericAPIView):
                 return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
 
             video = request.FILES["video"]
+            # here we should check if the image is a video
+            if video:
+                filename = video.name
+                print(filename)
+
+                if  (filename.endswith('.MP4') or filename.endswith('.mp4')) == False :
+                    datos = {'success':False,'data':"file is not of type .mp4"}
+                    return response.Response(datos, status=status.HTTP_201_CREATED)
+
+
+
+
+
+
 
 
             video_serializer = Videoerialiser(data=jd, context={'user': user_obj, 'vid' : video, 'request': request})
