466afd4af5dc201098e6c4c398edb3bc308f023e
angel zambrano <angeldzambrano99@gmail.com>
2022-11-11 09:36:46 -0800

    modified

--- proyecto_musica/chat/consumers.py
+++ proyecto_musica/chat/consumers.py
@@ -23,7 +23,9 @@ class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
         self.user = self.scope["user"]
-        print(self.user)
+        print("self user", self.user)
+        print("self.scope", self.scope["user"])
+        
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
