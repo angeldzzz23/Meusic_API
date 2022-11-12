bb44a4485cda173681b252781d250d577ad051de
angel zambrano <angeldzambrano99@gmail.com>
2022-11-11 16:13:59 -0800

    added

--- proyecto_musica/chat/views.py
+++ proyecto_musica/chat/views.py
@@ -76,7 +76,6 @@ class InboxView(GenericAPIView):
         async_to_sync(channel_layer.send)("chat_338217561529", { "type": "send.alert"})
         print("here2")
         async_to_sync(channel_layer.send)("338217561529", { "type": "send.alert"})
-        async_to_sync(channel_layer.send)(338217561529, { "type": "send.alert"})
 
         print("here3")
         async_to_sync(channel_layer.send)("chat", { "type": "send.alert"})
