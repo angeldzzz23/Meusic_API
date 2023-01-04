874033a0dbea784b534528646e1573847433a726
angel zambrano <angeldzambrano99@gmail.com>
2023-01-04 01:13:01 -0800

    changed the amount of days the token has

--- proyecto_musica/proyecto_musica/settings.py
+++ proyecto_musica/proyecto_musica/settings.py
@@ -193,8 +193,8 @@ AUTH_USER_MODEL = 'authentication.User' # setting the auth user
 
 # implementing the simple_JWT
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=3),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=18),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': True,
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
