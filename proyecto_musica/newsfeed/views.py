d425ea15ac4c5f4bf2815a5a8d9eff78385d2a30
angel zambrano <angeldzambrano99@gmail.com>
2023-01-17 09:54:03 -0800

    added the endpoint for getting the public profile based on the username

--- proyecto_musica/newsfeed/views.py
+++ proyecto_musica/newsfeed/views.py
@@ -2,17 +2,34 @@ from django.shortcuts import render
 import json
 from django.http.response import JsonResponse
 from rest_framework.generics import GenericAPIView
-
+from rest_framework import response, status, permissions
 
 from rest_framework import response
+
+from authentication.models import User
+from authentication.serializers import RegisterSerializer
 
 
 class SeeProfileOfUserView(GenericAPIView):
 
+    # this returns the public profile of a user in the newsfeed
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request,id):
-        print(id)
 
-        datos = {'codigo':"200",'message': "Users not found..."}
-        return JsonResponse(datos)
-        esponse.Response(datos, status=status.HTTP_201_CREATED)
+        try:
+            user = User.objects.get(username=id)
+
+        except User.DoesNotExist:
+            res = {'success' : True, 'user': None}
+            return response.Response(res, status=status.HTTP_200_OK)
+
+
+        serializer = RegisterSerializer(user)
+        serialized_data = (serializer.data).copy()
+        if 'gender' in serializer.data:
+            serialized_data.pop('gender')
+
+        res = {'success' : True, 'user': serialized_data}
+
+        return response.Response(res, status=status.HTTP_201_CREATED)
