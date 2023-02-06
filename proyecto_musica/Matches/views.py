b9268f2c950b3997eb976b210b6da3745c0063ca
angel zambrano <angeldzambrano99@gmail.com>
2023-02-06 11:56:49 -0800

    created the matches endpoint

--- proyecto_musica/Matches/views.py
+++ proyecto_musica/Matches/views.py
@@ -6,7 +6,10 @@ from rest_framework import response, status, permissions
 from rest_framework import response
 from authentication.models import User
 
+from newsfeed.models import User_Matches, User_Likes
 
+
+from Matches.serializers import MatchesSerializer
 
 class MatchesView(GenericAPIView):
     # this will get all of the matches
@@ -17,8 +20,52 @@ class MatchesView(GenericAPIView):
         CurrentUser = request.user
 
 
+        # will get all of the matches that the user has
+          # this matches have to be where is_active is true
 
-        res = {'success' : True, 'hello ': 'world'}
+        userMatchWithUserBeingMatched = User_Matches.objects.filter( other_user=CurrentUser)
+
+        userMatchWithUserMatching = User_Matches.objects.filter(current_user=CurrentUser)
+
+
+        combinedMatches = (userMatchWithUserBeingMatched | userMatchWithUserMatching).order_by('-created_at')
+
+        url = request.build_absolute_uri()
+        newurl = str(url)
+
+        base_url = newurl[:-8] + ''
+
+        # for every match we must return:
+        # the video of the user
+        # username
+        # and the message sent
+
+        user_objects = []
+
+        for match in combinedMatches:
+            matchedUser = None
+
+            if match.current_user == CurrentUser:
+                matchedUser = match.other_user
+            else:
+                matchedUser = match.current_user
+
+
+            serializer = MatchesSerializer(matchedUser, context = {'request': request, 'base_url': base_url})
+
+
+            # getting the matches
+            theLike = User_Likes.objects.filter(userLiking=matchedUser, userTwo=CurrentUser)[0]
+
+
+            # refactor this
+            serialized_data = serializer.data
+            serialized_data['message'] = theLike.message
+
+            user_objects.append(serialized_data)
+
+
+        res = {'success' : True, 'Matches ': user_objects}
 
         return response.Response(res, status=status.HTTP_200_OK)
 
--- proyecto_musica/proyecto_musica/settings.py
+++ proyecto_musica/proyecto_musica/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK={
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
      'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 10,
 }
 
 
