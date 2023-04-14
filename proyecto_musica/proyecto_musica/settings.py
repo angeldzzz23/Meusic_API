56fc9f9c44df28c6b4ca1aa133ca2cfc4ef6c2ae
angel zambrano <angeldzambrano99@gmail.com>
2023-04-14 09:23:48 -0700

    added pagination

--- proyecto_musica/proyecto_musica/settings.py
+++ proyecto_musica/proyecto_musica/settings.py
@@ -164,6 +164,9 @@ REST_FRAMEWORK={
         'authentication.jwt.JWTAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 40
+
 }
 
 
