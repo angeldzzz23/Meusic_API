dbe36b4298a5a6f3fc2d00027cfb28505bc35423
Danil Merinov <danilmerinov9@gmail.com>
2023-09-11 13:02:16 -0400

    refactorAPI
    refactorAPI

--- proyecto_musica/api/views.py
+++ proyecto_musica/api/views.py
@@ -1,5 +1,3 @@
-#from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
 from django.views import View
 from django.http.response import JsonResponse
 from django.utils.decorators import method_decorator
@@ -11,38 +9,24 @@ from authentication.models import User
 import pathlib
 from PIL import Image
 from django.core import serializers
-
 from api.models import Images, Videos
-#pathlib.Path('save_path').mkdir(parents=True, exist_ok=True)
 from proyecto_musica.settings import MEDIA_URL
-
-# the picture serializer
-
 from api.serializers import PictureSerialiser
 from api.serializers import PicturesSerializer
 from api.serializers import Videoerialiser
 from api.serializers import VideosSerializer
-
 from rest_framework import response, status, permissions
+from rest_framework.generics import GenericAPIView
+from ranged_fileresponse import RangedFileResponse
+from django.conf import settings
+import os
+from api.functions import validate_request, validate_image_title, deleteImage, deleteVideo, postVideo
 
 
-#import mimetypes
-#from django.http import HttpResponse
-#from rest_framework import response, status, permissions
-#from django.contrib.auth import authenticate
-
-# add
-# TODO: add an update method
 
 class UpdateImage(GenericAPIView):
-
     serializer_class= PictureSerialiser
     permission_classes = (permissions.IsAuthenticated,)
-
-    # create serializer
-    # TODO: Ignore messy code
-
-    # this returns all of the imags that bellong to a user
 
     def get(self,request):
         user = request.user
@@ -51,56 +35,28 @@ class UpdateImage(GenericAPIView):
         return response.Response(res)
 
 
-    # deleting the image
     def delete(self, request,id=None):
         user = request.user
         img = Images.objects.filter(image_id=id, user=request.user.id)
-
-        if len(img) == 0:
-            res = {'success' : False, 'error': 'image with that id does not exist'}
-            return response.Response(res)
-
-        imgeObj = img[0]
-        imgeObj.image.delete()
-        imgeObj.delete()
-
-
+        deleteImageResponse = deleteImage(img)
+        if deleteImageResponse is not None: return deleteImageResponse
 
         serializer = PicturesSerializer(user)
-
         res = {'success' : True, 'data': serializer.data}
         return response.Response(res)
 
 
-    # this post method requires an id
-    # creates an an image record for the user
-        # in the case of when the user already has an image saved in the specific spot
-            # we override the image
     def post(self,request,id=None):
         jd = request.data
+        validData = functions.validate_request(jd)
+        if validData is not None: return validData
 
-        #making sure that the body is only 2
-        if len(jd) != 2:
-            res = {'success' : False, 'error': "you didnt add all of the parameters"}
-            return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
-
-        if "title" not in jd:
-            res = {'success' : False, 'error' : "Title has to be specified in the request as a key"}
-            return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
-
-        if "image" not in jd:
-            res = {'success' : False, 'error' : "Image has to be specified in the request as a key"}
-            return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
-
-
-        #verifying that we have a valid user
         try:
             user_obj = request.user
         except User.DoesNotExist:
             res = {'success' : False, 'error' : "User id does not exist."}
             return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
 
-        # verify that im uploadin a valid image
         img = request.FILES["image"]
 
         try:
@@ -111,101 +67,51 @@ class UpdateImage(GenericAPIView):
             return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
 
         newpic = Images(user=user_obj, title=jd['title'], image = img)
-        # TODO: imeplement enums
-        valid_titles = ["image_1","image_2","image_3","image_4","image_5","image_6", "profile_image"]
+        validDataImage = validate_image_title(jd)
+        if validDataImage is not None: return validDataImage
 
-        # making sure that we have a valid title name
-        if jd['title'] not in valid_titles:
-            res = {'success' : False, 'error': "not a valid image title"}
-            return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
-
-        # does the user already have an image_1, if they do then we
         CurrentUsrimages = Images.objects.filter(user=user_obj, title=jd['title'])
-
         picture_serializer = PictureSerialiser(data=jd, context={'user': user_obj, 'img' : img, 'request': request})
-
         if picture_serializer.is_valid():
-            # implementation goes here
             picture_serializer.save()
             datos = {'success':True,'data':picture_serializer.data}
-
             return response.Response(datos, status=status.HTTP_201_CREATED)
         else:
-                datos = {'codigo':"200",'message': "success", "url": newpic.url}
-                return JsonResponse(datos)
-
-
-
-from ranged_fileresponse import RangedFileResponse
-
-from django.conf import settings
-import os
-
+            datos = {'codigo':"200",'message': "success", "url": newpic.url}
+            return JsonResponse(datos)
 
 
 class LoadVideo(GenericAPIView): 
-
     def get(self, request): 
-        video_path = "sample_vid.mp4"  # Update with your video file path
+        video_path = "sample_vid.mp4" 
         file_size = os.path.getsize(video_path)
-
         response = RangedFileResponse(request, open(video_path, "rb"), content_type="video/mp4")
-
         return response
-
-        # res = {'hello': 'world'}
-        # image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sample_vid.mp4')
-        # print('file path', image_path)
-        print(request.data)
-
-        # filename = 'sample_vid.mp4'
-        # response = RangedFileResponse(request, open(filename, 'r'), content_type='video/mp4')
-        # response['Content-Disposition'] = 'attachment; filename="%s"' % filename
-        # return response
-
-        # return response.Response({'hello':'world'})
 
 class UpdateVideo(GenericAPIView):
         serializer_class= PictureSerialiser
         permission_classes = (permissions.IsAuthenticated,)
-
-
         def get(self,request):
             user = request.user
             serializer = VideosSerializer(user)
             res = {}
             res['success'] = True
             res['videos'] = serializer.data['videos']
-      
             res = res
-
             return response.Response(res)
 
-
-        # adding the delete methodb
 
         def delete(self, request, id=None):
             vid = Videos.objects.filter(video_id=id, user=request.user.id)
-
-
-            if len(vid) == 0:
-                res = {'success' : False, 'error': 'video with that id does not exist'}
-                return response.Response(res)
-
-            videoObj = vid[0]
-            videoObj.video.delete()
-            videoObj.delete()
-
+            deleteVideoResponse = deleteVideo(vid)
+            if deleteVideoResponse is not None: return deleteVideoResponse
             res = {'success' : True, 'videos': []}
-
             return response.Response(res)
 
-        # im only going to allow intro Videos
+
         def post(self,request,id=None):
             jd = request.data
-
             caption = None
-
             if 'caption' in jd:
                 caption = jd['caption']
 
@@ -217,44 +123,24 @@ class UpdateVideo(GenericAPIView):
 
     
             if "video" not in jd:
-                # check if a video existss
                 currentUsrVideo = Videos.objects.filter(user=request.user)
-
-
-                if not currentUsrVideo:
-                    res = {'success' : False, 'error' : "Request must contain video as a key."}
-                    return response.Response(res, status=status.HTTP_400_BAD_REQUEST) 
-
-
-                if len(currentUsrVideo) < 0:
-                    res = {'success' : False, 'error' : "Request must contain video as a key."}
-                    return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
+                postVideoResponse = postVideo(currentUsrVideo)
+                if postVideoResponse is not None: return postVideoResponse
                 else:                    
-                    # video exists in the server and we're just changing the caption
                     currentUsrVideo[0].caption = caption
                     currentUsrVideo[0].save()
-
-                    # return the newest user 
                     serializer = VideosSerializer(request.user)
                     res = {}
                     res['success'] = True
                     res['video'] = serializer.data['videos'][0]
                     del res['video']['title'] 
-                   
                     res = res
-
                     return response.Response(res)
-
 
                 res = {'success' : False, 'error' : "Request must contain video as a key."}
                 return response.Response(res, status=status.HTTP_400_BAD_REQUEST)
 
             video = request.FILES["video"]
-
-
-
-
-            # here we should check if the image is a video
             if video:
                 filename = video.name
 
@@ -264,16 +150,11 @@ class UpdateVideo(GenericAPIView):
 
 
             video_serializer = Videoerialiser(data=jd, context={'user': user_obj, 'vid' : video, 'request': request, 'caption': caption})
-
             if video_serializer.is_valid():
                 video_serializer.save()
-
                 res = {}
                 res['success'] = True
                 res['video'] = video_serializer.data
-
-
-
                 datos = res
                 return response.Response(datos, status=status.HTTP_201_CREATED)
             else:
